673a55f6e4ffeda3d6e7cc6288f0752ad61460e1
Cristina Arango Escobar <cristinarangoe@hotmail.com>
2021-11-14 23:49:34 -0500

    arreglo fibo con = 0

--- src/fibonacci/dto/create-fibonacci.dto.ts
+++ src/fibonacci/dto/create-fibonacci.dto.ts
@@ -2,6 +2,6 @@ import { IsInt, Min } from 'class-validator';
 
 export class CreateFibonacci {
   @IsInt({ message: 'n debe ser numero entero' })
-  @Min(1, { message: 'n debe ser superior o igual a 1' })
+  @Min(0, { message: 'n debe ser superior o igual a 0' })
   n: number;
 }
